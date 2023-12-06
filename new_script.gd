cef3bb03a23d9d06e6697dd12e5afb8b7909e414
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2023-12-06 17:47:13 -0600

    Test push

--- new_script.gd
+++ new_script.gd
@@ -5,6 +5,6 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
 	pass
