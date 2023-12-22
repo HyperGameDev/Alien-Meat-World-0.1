0c3d5581a07b457e0d6cb8838a1c11364fd49906
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2023-12-21 21:09:57 -0600

    Run And Walk improved; Added HP debug
    Run and walk anims have arm movement, and blend better together. Hitpoint debug works by pressing the 1 key allowing for toggle of HP numbers. Made some adjustments to terrain block positions, but still need to get the chasm position set up right.

---
+++ Debug.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var dmg_label_ArmL = $"../Player/Alien/Armature/Skeleton3D/Alien_ArmL/Dmg_Label"
+@onready var dmg_label_ArmR = $"../Player/Alien/Armature/Skeleton3D/Alien_ArmR/Dmg_Label"
+@onready var dmg_label_Body = $"../Player/Alien/Armature/Skeleton3D/Alien_Body/Dmg_Label"
+@onready var dmg_label_LegL = $"../Player/Alien/Armature/Skeleton3D/Alien_LegL/Dmg_Label"
+@onready var dmg_label_LegR = $"../Player/Alien/Armature/Skeleton3D/Alien_LegR/Dmg_Label"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _input(event):
+	if event.is_action_pressed("Debug 1"):
+		dmg_label_ArmL.visible = !dmg_label_ArmL.visible
+		dmg_label_ArmR.visible = !dmg_label_ArmR.visible
+		dmg_label_LegL.visible = !dmg_label_LegL.visible
+		dmg_label_LegR.visible = !dmg_label_LegR.visible
+		dmg_label_Body.visible = !dmg_label_Body.visible
+
