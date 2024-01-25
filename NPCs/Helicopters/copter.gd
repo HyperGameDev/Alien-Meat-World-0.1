a1219eee31048d3207fae1e201ea9196de35585d
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2024-01-24 21:33:47 -0600

    Helicopters spawn and approach player
    Helicopter script is more separate from the spawner script now.
    
    The helicopters stop before getting to close to the Player.
    
    My2Cents helped me set up the helicopter movement to be based on velocity instead of just position-moving. This is to prepare for using a NavigationAgent3D apparently... That will in theory help the units avoid each other.
    
    Had to flip around the helicopter model in godot so it can use "look_at" to always follow the player via rotation, which apparently inverts the direction.
    
    Added an "empty" dirt block and renamed the "empty" grass one to add more clear movement to the terrain when nothing is on it.

--- NPCs/Helicopters/copter.gd
+++ NPCs/Helicopters/copter.gd
@@ -1,10 +1,44 @@
 extends Node3D
 
+class_name Copter
+
+var copter_pos: Vector3
+var copter_x_pos_min = -15
+var copter_x_pos_max = 15
+
+var copter_spawn_z_pos = -130
+
+var speed = 10
+
+var velocity = Vector3.ZERO
+
+var is_moving = true
+
+@onready var copter_area = $Area_Copter
+@onready var player = get_tree().get_current_scene().get_node("Player/DetectionAreas/Area_Player-Proximity")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("propeller_speed-01")
+	player.area_entered.connect(copter_stop)
+	var copter_x_pos = randf_range(copter_x_pos_min,copter_x_pos_max)
+	var copter_x_offset = Vector3(copter_x_pos, 0, 0)
+	copter_pos = Vector3(0,2,copter_spawn_z_pos)
+	global_position = copter_pos + copter_x_offset
+#
+#	print("Trying to move copter ", "(", global_position.z, ") ", "to ", "Player at ", player.global_position.z)
 
+
+func _physics_process(delta):
+	look_at(player.global_position)
+	if is_moving:
+		var direction = (player.global_position - global_position).normalized()
+		velocity = direction * speed
+		global_position += velocity * delta 
+	
+func copter_stop(area):
+	if area == copter_area:
+		is_moving = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- NPCs/NPC Spawns.gd
+++ NPCs/NPC Spawns.gd
@@ -1,8 +1,7 @@
-extends Node3D
+extends Node
 
 var spawn_interval_min = 3
-var spawn_interval_max = 10
-var copter_spawn_pos = Vector3(0,0,-25)
+var spawn_interval_max = 5
 
 @onready var spawn_interval_timer : Timer = Timer.new()
 
@@ -12,7 +11,7 @@ func _ready():
 	spawn_interval_timer.timeout.connect(spawn_helicopter)
 	spawn_interval_timer.one_shot = true
 	add_child(spawn_interval_timer)
-	spawn_helicopter()
+	spawn_interval_timer.start(randi_range(spawn_interval_min,spawn_interval_max))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func spawn_helicopter():
@@ -21,4 +20,3 @@ func spawn_helicopter():
 	
 	var copter = load("res://NPCs/Helicopters/copter_001.tscn").instantiate()
 	get_tree().get_current_scene().add_child(copter)
-	copter.set_position(copter_spawn_pos)
