94cd1dae724d63ae736da3415642de9f242daea1
Lýz <147095077+HyperGameDev@users.noreply.github.com>
2024-05-29 18:53:12 -0500

    Commented out an unperformant line
    Process function in the particle fx (was being used to control the helicopter spinout particle fx.

--- Objects/Buildings/Houses/Particles_destruction.gd
+++ Objects/Buildings/Houses/Particles_destruction.gd
@@ -10,4 +10,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_rotation = Vector3.ZERO
+	#global_rotation = Vector3.ZERO
+	pass
